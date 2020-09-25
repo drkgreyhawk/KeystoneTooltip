4dbd6e971693021c8c23dfd1901804b0c60bdc01
drkgreyhawk <drkgreyhawk@gmail.com>
2020-09-25 14:04:27 -0600

    Localization issues fixed

--- Locales/ruRU.lua
+++ Locales/ruRU.lua
@@ -1,4 +1,4 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("KeystoneLoot", "ruRU", true)
+local L = LibStub("AceLocale-3.0"):NewLocale("KeystoneLoot", "ruRU")
 
 L["KeystoneLoot: Loaded"] = "KeystoneLoot: Загружен"
 L["KeystoneLoot: Enabled"] = "KeystoneLoot: Включен"
