8df9ef3df41e5fd954265394f66c916314ef6797
drkgreyhawk <drkgreyhawk@gmail.com>
2020-12-15 10:26:13 -0700

    community provided localization

---
+++ Locales/zhTW.lua
@@ -0,0 +1,14 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("KeystoneLoot", "ruRU")
+
+if L then
+
+L["KeystoneLoot does not have any function command."] = "傳奇+ 獎勵提示沒有任何功能指令。",
+L["KeystoneLoot: Disabled"] = "傳奇+ 獎勵提示: 已停用",
+L["KeystoneLoot: Enabled"] = "傳奇+ 獎勵提示: 已啟用",
+L["KeystoneLoot: Loaded"] = "傳奇+ 獎勵提示: 已載入",
+L["Loot Item Level: "] = "戰利品物品等級: ",
+L["This shows the level of the item you'll find in this week's chest."] = "這是每週寶箱開出的物品等級。",
+L["Weekly Chest Item Level: "] = "每週寶箱物品等級: ",
+L["Weekly Chest Reward"] = "每週寶箱獎勵"
+
+end
