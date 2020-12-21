31d8197e5eeac166b76a63772171e74bc16266f9
Anibal Fernandez <afernandez@grupolegal.com.pa>
2020-12-21 09:19:23 -0500

    Added spanish (es-MX) Locale, this file could be used for Spain spanish, creating the respective file.

---
+++ Locales/esMX.lua
@@ -0,0 +1,16 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("KeystoneLoot", "esMX")
+
+if L then
+
+L["KeystoneLoot: Loaded"] = "KeystoneLoot: Cargado"
+L["KeystoneLoot: Enabled"] = "KeystoneLoot: Habilitado"
+L["KeystoneLoot: Disabled"] = "KeystoneLoot: Deshabilitado"
+L["KeystoneLoot does not have any function command."] = "KeystoneLoot no tiene ningún comandos de función."
+
+L["This shows the level of the item you'll find in this week's chest."] = "Esto muestra el nivel del objeto que encontrarás en el cofre de esta semana"
+L["Weekly Chest Reward"] = "Recompensa del Cofre Semanal"
+
+L["Loot Item Level: "] = "Nivel de Objeto del Botín"
+L["Weekly Chest Item Level: "] = "Nivel de Objeto del Cofre Semanal"
+
+end
