3db2e9417fa1d6f78c84956273c12fc0317c4e7d
Anibal Fernandez <afernandez@grupolegal.com.pa>
2020-12-21 09:21:15 -0500

    Fixed a typo

--- Locales/esMX.lua
+++ Locales/esMX.lua
@@ -5,7 +5,7 @@ if L then
 L["KeystoneLoot: Loaded"] = "KeystoneLoot: Cargado"
 L["KeystoneLoot: Enabled"] = "KeystoneLoot: Habilitado"
 L["KeystoneLoot: Disabled"] = "KeystoneLoot: Deshabilitado"
-L["KeystoneLoot does not have any function command."] = "KeystoneLoot no tiene ningún comandos de función."
+L["KeystoneLoot does not have any function command."] = "KeystoneLoot no tiene ningún comando de función."
 
 L["This shows the level of the item you'll find in this week's chest."] = "Esto muestra el nivel del objeto que encontrarás en el cofre de esta semana"
 L["Weekly Chest Reward"] = "Recompensa del Cofre Semanal"
