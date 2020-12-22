1981524b91c2845e020a037c51250a13a8acae5e
Hyrum Mack <drkgreyhawk@gmail.com>
2020-12-21 19:11:13 -0700

    Update koKR.lua

--- Locales/koKR.lua
+++ Locales/koKR.lua
@@ -2,13 +2,13 @@ local L = LibStub("AceLocale-3.0"):NewLocale("KeystoneLoot", "koKR")
 
 if L then
 
-L["KeystoneLoot does not have any function command."] = "KeystoneLoot에는 사용할 수 없는 명령어입니다.",
-L["KeystoneLoot: Disabled"] = "KeystoneLoot: 비활성화",
-L["KeystoneLoot: Enabled"] = "KeystoneLoot: 활성화",
-L["KeystoneLoot: Loaded"] = "KeystoneLoot: 불러오기",
-L["Loot Item Level: "] = "전리품 아이템 레벨: ",
-L["This shows the level of the item you'll find in this week's chest."] = "이것은 당신이 이번 주 상자에 찾을 수 있는 장비의 레벨을 나타냅니다.",
-L["Weekly Chest Item Level: "] = "금고 첫번째 보상 아이템 레벨: ",
+L["KeystoneLoot does not have any function command."] = "KeystoneLoot에는 사용할 수 없는 명령어입니다."
+L["KeystoneLoot: Disabled"] = "KeystoneLoot: 비활성화"
+L["KeystoneLoot: Enabled"] = "KeystoneLoot: 활성화"
+L["KeystoneLoot: Loaded"] = "KeystoneLoot: 불러오기"
+L["Loot Item Level: "] = "전리품 아이템 레벨: "
+L["This shows the level of the item you'll find in this week's chest."] = "이것은 당신이 이번 주 상자에 찾을 수 있는 장비의 레벨을 나타냅니다."
+L["Weekly Chest Item Level: "] = "금고 첫번째 보상 아이템 레벨: "
 L["Weekly Chest Reward"] = "금고 첫번째 보상"
 
 end
